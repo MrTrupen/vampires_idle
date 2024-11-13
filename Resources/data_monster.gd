296531b8d40e092000f374052dbcb93e1b0cf4a8
Trupen <MrTrupen@gmail.com>
2024-11-13 18:03:23 +0100

    Added names and experience to monster resource

--- Resources/data_monster.gd
+++ Resources/data_monster.gd
@@ -1,10 +1,13 @@
 class_name DataMonster
 extends Resource
 
+@export var name: String = "Monster"
+
 @export var sprite: Texture2D
 
 @export var movement_speed: int = 1
 @export var health: int = 1
+@export var experience: int = 1
 @export var damage: int = 1
 ## Attack cooldown in seconds
 @export var attack_cooldown: float = 1
